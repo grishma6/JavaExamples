da37b82cfa3f1210fa9548775d6416264fa6308c
grishma6 <gxg59150@ucmo.edu>
2025-09-05 15:30:22 -0500

    Sorting an Array Ascending Order

---
+++ src/SortanArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class SortanArray {
+    public static void main(String[] args){
+
+        int[] num = {3,4,6,7,9};
+
+        Arrays.sort(num);
+
+        System.out.println("Sorting the Arrays in ascending order: " + Arrays.toString(num));
+
+    }
+}
