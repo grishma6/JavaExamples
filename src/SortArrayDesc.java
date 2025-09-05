ae53806759403488540a2cccd5983b15b8e8c403
grishma6 <gxg59150@ucmo.edu>
2025-09-05 15:53:19 -0500

    Sorting an Array Descending Order

--- src/SortArrayDesc.java
+++ src/SortArrayDesc.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class SortArrayDesc {
-}
+    public static void main(String[] args) {
+        int[] num = {8, 9, 5, 7, 3, 2};
+
+        //Arrays.sort(num, Collections.reverseOrder());
+
+        //System.out.println("Sorting the Array in Descending Order: " + Arrays.toString(num));
+
+        int n = num.length;
+
+        Arrays.sort(num);
+        for(int i=0; i<n/2; i++){
+            int temp = num[i];
+            num[i] = num[n-i-1];
+            num[n-i-1] = temp;
+        }
+            System.out.println("Sorting the Array in Descending Order: " + Arrays.toString(num));
+        }
+    }
